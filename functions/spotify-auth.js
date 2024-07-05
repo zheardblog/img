3339ac41ff8294ca3d4619e00495406f042088a7
CompuNetsi <unifenix01@gmail.com>
2024-07-05 13:25:26 -0500

    Update spotify-auth.js

--- functions/spotify-auth.js
+++ functions/spotify-auth.js
@@ -2,38 +2,46 @@ const axios = require('axios');
 const querystring = require('querystring');
 
 exports.handler = async (event, context) => {
-    const code = JSON.parse(event.body).code;
-    const clientId = process.env.CLIENT_ID;
-    const clientSecret = process.env.CLIENT_SECRET;
-    const redirectUrl = process.env.REDIRECT_URL;
+    // Permitir solicitudes solo si es una solicitud preflight OPTIONS o POST
+    if (event.httpMethod === 'OPTIONS' || event.httpMethod === 'POST') {
+        const code = JSON.parse(event.body).code;
+        const clientId = process.env.CLIENT_ID;
+        const clientSecret = process.env.CLIENT_SECRET;
+        const redirectUrl = process.env.REDIRECT_URL;
 
-    try {
-        const response = await axios.post('https://accounts.spotify.com/api/token', querystring.stringify({
-            grant_type: 'authorization_code',
-            code: code,
-            redirect_uri: redirectUrl,
-            client_id: clientId,
-            client_secret: clientSecret
-        }));
+        try {
+            const response = await axios.post('https://accounts.spotify.com/api/token', querystring.stringify({
+                grant_type: 'authorization_code',
+                code: code,
+                redirect_uri: redirectUrl,
+                client_id: clientId,
+                client_secret: clientSecret
+            }));
 
+            return {
+                statusCode: 200,
+                headers: {
+                    'Access-Control-Allow-Origin': 'https://www.zheard.net', // Permitir acceso solo desde este origen
+                    'Access-Control-Allow-Headers': 'Content-Type',
+                    'Access-Control-Allow-Methods': 'POST'
+                },
+                body: JSON.stringify({ accessToken: response.data.access_token })
+            };
+        } catch (error) {
+            return {
+                statusCode: 500,
+                headers: {
+                    'Access-Control-Allow-Origin': 'https://www.zheard.net', // Permitir acceso solo desde este origen
+                    'Access-Control-Allow-Headers': 'Content-Type',
+                    'Access-Control-Allow-Methods': 'POST'
+                },
+                body: 'Error fetching token'
+            };
+        }
+    } else {
         return {
-            statusCode: 200,
-            headers: {
-                'Access-Control-Allow-Origin': '*', // Permitir acceso desde cualquier origen
-                'Access-Control-Allow-Headers': 'Content-Type',
-                'Access-Control-Allow-Methods': 'POST'
-            },
-            body: JSON.stringify({ accessToken: response.data.access_token })
-        };
-    } catch (error) {
-        return {
-            statusCode: 500,
-            headers: {
-                'Access-Control-Allow-Origin': '*', // Permitir acceso desde cualquier origen
-                'Access-Control-Allow-Headers': 'Content-Type',
-                'Access-Control-Allow-Methods': 'POST'
-            },
-            body: 'Error fetching token'
+            statusCode: 405,
+            body: 'Method Not Allowed'
         };
     }
 };
